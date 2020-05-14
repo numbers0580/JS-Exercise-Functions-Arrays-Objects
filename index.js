845122a1acbe9307c6f39607833b5ef4ae1ab092
numbers0580 <koreankid18@yahoo.com>
2020-05-14 13:12:15 -0600

    Completed first few challenges, so far. Saving progress.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function addNumbers(num1, num2) {
  * ### Challenge `sayGoodbye`
  * 
  * @instructions
- * This function should take an a name as an argument,
+ * This function should take in a name as an argument
  * and return a string that says 'Goodbye, {name}. Have a great day.'
  * 
  * For example, if we invoke `sayGoodbye`
@@ -36,9 +36,12 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(enteredName) {
+  return "Goodbye, " + enteredName + ". Have a great day.";
 }
+console.log("Displaying sayGoodbye() function passing in 'Moonmen' for the name");
+console.log(sayGoodbye('Moonmen'));
+console.log("That was a 'Rick & Morty' reference for anyone wondering.");
 
 /**
  * ### Challenge `temperatureCtoF`
@@ -54,9 +57,13 @@ function sayGoodbye(/* code here */) {
  * Hint 1: The formula for converting celsius to fahrenheit is t*9/5 + 32 where t is the temperature in celsius.
  * Hint 2: There is a very easy way to round numbers in JS. Do a google search to find out how. 
 */
-function temperatureCtoF(/* code here */) {
-  /* code here */
+function temperatureCtoF(celsius) {
+  let fahrneheit = Math.round(celsius * 1.8 + 32);
+
+  return fahrneheit;
 }
+console.log("Testing temperatureCtoF() function passing in '32' below:");
+console.log(temperatureCtoF(32));
 
 /**
  * ### Challenge `temperatureInF`
@@ -75,17 +82,31 @@ function temperatureCtoF(/* code here */) {
  * 
  * Hint: You can call your `temperatureCtoF` function from inside `temperatureInF`.
 */
-function temperatureInF(/* code here */) {
-  /* code here */
+function temperatureInF(degree, cOrF) {
+  if((cOrF === 'C') || (cOrF === 'c')) {
+    let fahrenheit = Math.round(degree * 1.8 + 32);
+    return fahrenheit + "F";
+  } else if((cOrF === 'F') || (cOrF === 'f')) {
+    let fahrenheit = Math.round(degree);
+    return fahrenheit + "F";
+  } else {
+    return "Unrecognized temperature. Please enter units in (C)elsius or (F)ahrenheit only.";
+  }
 }
+console.log("Testing temperatureInF() function passing in '23, C' below");
+console.log(temperatureInF(23, 'C'));
+console.log("Testing temperatureInF() function passing in '57, f' below");
+console.log(temperatureInF(57, 'f'));
+console.log("Testing temperatureInF() function passing in '18, G' below");
+console.log(temperatureInF(18, 'G'));
 
 
 /**
  * ### Challenge `makePersonObject`
  * 
  * @instructions
- * This function should take an id, a name and an email as arguments,
- * and return an object with `id`, `name` and `email` properties.
+ * This function should take an id, a name, and an email as arguments
+ * and return an object with `id`, `name`, and `email` properties.
  * 
  * For example, if we invoke `makePersonObject`
  * passing 5, 'Leia' and 'leia@leia.com' as arguments,
@@ -96,9 +117,26 @@ function temperatureInF(/* code here */) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  let position = 0;
+  let foundChar = false;
+  while((position < email.length) && (!foundChar)) {
+    if(email.charAt(position) === '@') {
+      foundChar = true;
+    }
+    position++;
+  }
+
+  if(foundChar) {
+    return {id, name, email};
+  } else {
+    return {id, name, 'email':"Invalid Email"};
+  }
 }
+console.log("Testing makePersonObject() function passing in '51780, Peter Wood, peters_email@peters_domain.com' below");
+console.log(makePersonObject(51780, 'Peter Wood', 'peters_email@peters_domain.com'));
+console.log("Testing makePersonObject() function passing in '3549, Guy Fieri, tripleD&food_network.net' below");
+console.log(makePersonObject(3549, 'Guy Fieri', 'tripleD&food_network.net'));
 
 /**
  * ### Challenge `getName`
@@ -113,9 +151,16 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(enteredUser) {
+  let displayString = "Hello, my name is " + enteredUser.name;
+  return displayString;
 }
+
+const user = {
+  name: 'Dave'
+};
+console.log("Testing getName() function passing in 'Dave' via 'user' object below");
+console.log(getName(user));
 
 
 /**
@@ -181,7 +226,60 @@ var inventory = [
 */
 
 // Data from file 
-var data = require('data./inventory.js'); 
+//var data = require('data./inventory.js');
+/*module.exports = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 },
+  { id: 15, car_make: "Dodge", car_model: "Intrepid", car_year: 2000 },
+  { id: 16, car_make: "Mitsubishi", car_model: "Montero Sport", car_year: 2001 },
+  { id: 17, car_make: "Buick", car_model: "Skylark", car_year: 1987 },
+  { id: 18, car_make: "Geo", car_model: "Prizm", car_year: 1995 },
+  { id: 19, car_make: "Oldsmobile", car_model: "Bravada", car_year: 1994 },
+  { id: 20, car_make: "Mazda", car_model: "Familia", car_year: 1985 },
+  { id: 21, car_make: "Chevrolet", car_model: "Express 1500", car_year: 2003 },
+  { id: 22, car_make: "Jeep", car_model: "Wrangler", car_year: 1997 },
+  { id: 23, car_make: "Eagle", car_model: "Talon", car_year: 1992 },
+  { id: 24, car_make: "Toyota", car_model: "MR2", car_year: 2003 },
+  { id: 25, car_make: "BMW", car_model: "525", car_year: 2005 },
+  { id: 26, car_make: "Cadillac", car_model: "Escalade", car_year: 2005 },
+  { id: 27, car_make: "Infiniti", car_model: "Q", car_year: 2000 },
+  { id: 28, car_make: "Suzuki", car_model: "Aerio", car_year: 2005 },
+  { id: 29, car_make: "Mercury", car_model: "Topaz", car_year: 1993 },
+  { id: 30, car_make: "BMW", car_model: "6 Series", car_year: 2010 },
+  { id: 31, car_make: "Pontiac", car_model: "GTO", car_year: 1964 },
+  { id: 32, car_make: "Dodge", car_model: "Ram Van 3500", car_year: 1999 },
+  { id: 33, car_make: "Jeep", car_model: "Wrangler", car_year: 2011 },
+  { id: 34, car_make: "Ford", car_model: "Escort", car_year: 1991 },
+  { id: 35, car_make: "Chrysler", car_model: "300M", car_year: 2000 },
+  { id: 36, car_make: "Volvo", car_model: "XC70", car_year: 2003 },
+  { id: 37, car_make: "Oldsmobile", car_model: "LSS", car_year: 1997 },
+  { id: 38, car_make: "Toyota", car_model: "Camry", car_year: 1992 },
+  { id: 39, car_make: "Ford", car_model: "Econoline E250", car_year: 1998 },
+  { id: 40, car_make: "Lotus", car_model: "Evora", car_year: 2012 },
+  { id: 41, car_make: "Ford", car_model: "Mustang", car_year: 1965 },
+  { id: 42, car_make: "GMC", car_model: "Yukon", car_year: 1996 },
+  { id: 43, car_make: "Mercedes-Benz", car_model: "R-Class", car_year: 2009 },
+  { id: 44, car_make: "Audi", car_model: "Q7", car_year: 2012 },
+  { id: 45, car_make: "Audi", car_model: "TT", car_year: 2008 },
+  { id: 46, car_make: "Oldsmobile", car_model: "Ciera", car_year: 1995 },
+  { id: 47, car_make: "Volkswagen", car_model: "Jetta", car_year: 2007 },
+  { id: 48, car_make: "Dodge", car_model: "Magnum", car_year: 2008 },
+  { id: 49, car_make: "Chrysler", car_model: "Sebring", car_year: 1996 },
+  { id: 50, car_make: "Lincoln", car_model: "Town Car", car_year: 1999 }
+]*/
+
 
 /**
   * ### Example Array Challenge:
